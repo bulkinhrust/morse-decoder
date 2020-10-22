e97744aec53a75db80899d9f9aed30a0337160ac
Marina <m.khmelyova@integer-soft.ru>
2020-10-22 15:43:17 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const stringLength = expr.length / 10;
+    let result = '';
+
+    const chunkSubstr = function (str, size) {
+        const numChunks = Math.ceil(str.length / size);
+        const chunks = new Array(numChunks);
+
+        for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
+          chunks[i] = str.substr(o, size);
+        }
+
+        return chunks;
+    };
+
+    for (i = 0; i < stringLength; ++i) {
+        let letterCode = expr.substring(i * 10, i * 10 + 10);
+        if (isNaN(letterCode)) {
+            result += ' ';
+            continue;
+        }
+
+        let morseResult = '';
+        chunkSubstr(letterCode, 2).forEach((chunk) => {
+            if (chunk === '10') {
+                morseResult += '.';
+            } else if (chunk === '11') {
+              morseResult += '-';
+            }
+        });
+        result += MORSE_TABLE[morseResult];
+    }
+
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
